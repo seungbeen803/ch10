ee4ec4fe177b209eab435739c3ca5e55d81e6517
seungbeen <seungbin803@gmail.com>
2022-11-29 10:09:26 +0900

    RePermutation 중복 순열

--- ch10/RePermutation1.c
+++ ch10/RePermutation1.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#define R 3
+
+int b[R];
+
+void RePerPrint(void) {
+	int i;
+	
+	for (i = 0; i < R; i++) {
+		printf("%5d", b[i]);
+	}
+	printf("\n");
+}
+
+//Àç±ÍÈ£Ãâ
+void RePermutation(int a[], int n, int r) {
+	int i;
+	if (r == R) RePerPrint();
+	else for (i = 0; i < n; i++) { // i = 0 ~ 4
+		b[r] = a[i];
+		RePermutation(a, n, r + 1);
+		}
+}
 
 int main(void) {
-	int i, j, k;
-	for (i = 1; i <= 5; i++) {
-		for (j = 1; j <= 5; j++) {
-			for (k = 1; k <= 5; k++) {
-				printf("%d, %d, %d\n", i, j, k);
-			}
-		}
-	}
+	int a[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(a) / sizeof(int);
+
+	RePermutation(a, n, 0);
+
 	return 0;
 }
