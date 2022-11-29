534641d4b34373a2d0a6d8bfc3b1a9be3d7f84a2
seungbeen <seungbin803@gmail.com>
2022-11-29 09:03:36 +0900

    단순 알고리즘 5개 나열 했을 때

---
+++ ch10/RePermutation1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, j, k;
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+			for (k = 1; k <= 5; k++) {
+				printf("%d, %d, %d\n", i, j, k);
+			}
+		}
+	}
+	return 0;
+}
