dad2f4c436911cf2c79b3525934d051a284ce27c
seungbeen <seungbin803@gmail.com>
2022-11-30 14:05:42 +0900

    RePermutation2.c

---
+++ ch10/RePermutation2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define R 3
+
+int b[R];
+
+void RePerPrint(void) {
+	int i;
+	
+	for (i = 0; i < R; i++) {
+		printf("%5d", b[i]);
+	}
+	printf("\n");
+}
+
+void exchange(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+//Àç±ÍÈ£Ãâ
+void RePermutation(int a[], int n, int r) {
+	int i;
+	if (r == R) RePerPrint();
+	else for (i = 0; i < n; i++) { // i = 0 ~ 4
+		exchange(&a[0], &a[i]); // (a+0, a + i)
+		b[r] = a[0];
+		RePermutation(a, n, r + 1);
+		exchange(a+0, a+i);
+		}
+}
+
+int main(void) {
+	int a[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(a) / sizeof(int);
+
+	RePermutation(a, n, 0);
+
+	return 0;
+}
